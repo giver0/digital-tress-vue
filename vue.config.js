bee71f13773a8f8658538b9d9a9ac89241dc08ad
ivan.hryshko <ihryshko@datrics.ai>
2022-12-20 10:04:39 +0200

    feat: deleted public path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/digital-trees-vue/',
+  // publicPath: '/digital-trees-vue/',
   transpileDependencies: true,
 })
